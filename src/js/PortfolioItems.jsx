ce834598082172e429fd711bc9b0a8c49bdddcba
jolilj <lilja.joakim@gmail.com>
2016-08-22 22:28:00 +0200

    minor fix

--- src/js/PortfolioItems.jsx
+++ src/js/PortfolioItems.jsx
@@ -26,7 +26,7 @@ module.exports = {
     footer: (
       <div className="modal-footer">
         <a href="https://github.com/jolilj/SimpleMadgwickFilter/raw/master/project_report.pdf"
-         target="_blank">&nbsp;&nbsp;;
+         target="_blank">&nbsp;&nbsp;
         <i className="fa fa-file-pdf-o" aria-hidden="true"></i> &nbsp;Download report</a>
         <a href="https://github.com/jolilj/SimpleMadgwickFilter" target="_blank">&nbsp;&nbsp;&nbsp;&nbsp;
         <i className="fa fa-github-square fa-lg" aria-hidden="true"/> &nbsp;View project on Github</a>
@@ -64,7 +64,7 @@ module.exports = {
         </div>),
     footer: (
       <div className="modal-footer">
-        <a href="https://github.com/jolilj/CascadedCNNFaceDetection/raw/master/project_report.pdf" target="_blank">&nbsp;&nbsp;&nbsp;&nbsp;
+        <a href="https://github.com/jolilj/CascadedCNNFaceDetection/raw/master/project_report.pdf" target="_blank">&nbsp;&nbsp;
         <i className="fa fa-file-pdf-o" aria-hidden="true"></i> &nbsp;Download report</a>
         <a href="https://github.com/jolilj/CascadedCNNFaceDetection" target="_blank">&nbsp;&nbsp;&nbsp;&nbsp;
         <i className="fa fa-github-square fa-lg" aria-hidden="true"/> &nbsp;View project on Github</a>
